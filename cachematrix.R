0e7c6981b4661ff7001ad078437e26ec84eee28e
George Gonzalez <georgecgonzalez@gmail.com>
2018-06-03 17:17:55 -0500

    Added initial comments and created RStudio project file.

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,22 @@
-## Put comments here that give an overall description of what your
-## functions do
+## R Programming class on Coursera
+## Instructor: Roger Peng
 
-## Write a short comment describing this function
+## Programming assignment 2
+## Author: George Gonzalez
+
+## makeCacheMatrix creates a special "matrix' object with member functions
+## that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
-## Write a short comment describing this function
+## cacheSolve computes the inverse of the special "matrix" returned by
+## makeCacheMatrix above.
+
+## If the inverse has already been calculated (and the matrix has not changed)
+## then the cacheSolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
